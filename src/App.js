cb7ef93899a77afa4f074a48b8071705a6f406c8
Afifa-Aseer <61260444+Afifa-Aseer@users.noreply.github.com>
2022-02-18 01:16:04 +0500

    weather_app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Home from "./components/Home.jsx"
+import Weather from "./components/weather";
+
+
+function App(){
+  return(
+  <>  
+  {/* <Home/> */}
+  <Weather/>
+  </>
+  )
+}
+export default App;
